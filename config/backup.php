c9f47d01e71f426c4ade9a6e0d14299279c17c11
Ally Prof <allyprof@example.com>
2026-04-23 10:56:29 +0300

    Empty backup name to support direct folder ID

--- config/backup.php
+++ config/backup.php
@@ -14,7 +14,7 @@ use Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes;
 return [
 
     'backup' => [
-        'name' => 'Medallion',
+        'name' => '',
         'source' => [
             'files' => [
                 /*
